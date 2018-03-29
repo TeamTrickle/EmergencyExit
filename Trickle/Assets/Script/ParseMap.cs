8fe3aeeeb30a84f0be243ba393ba97e0ba8d30d9
Heewon Kim <nabicore@icloud.com>
2018-03-29 22:37:30 +0900

    Project File Initialize

---
+++ Trickle/Assets/Script/ParseMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(TextAsset))]
+public class ParseMap : MonoBehaviour {
+	TextAsset mapFile;
+
+	void Start() {
+
+	}
+}
